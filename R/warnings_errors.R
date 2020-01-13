4e91a5be714f2aa518da51bec9b8bf7c9c6dc53c
Zhian N. Kamvar <zkamvar@gmail.com>
2020-01-13 11:42:57 +0000

    update warnings

--- R/warnings_errors.R
+++ R/warnings_errors.R
@@ -2,6 +2,9 @@
 withWarnings <- function(expr) {
   myWarnings <- NULL
   myErrors   <- NULL
+  # NOTE: This function is properly scoped since myWarnings is defined directly
+  #       above. Thus, the <<- operator is valid and won't reach outside of
+  #       this scope.
   wHandler <- function(w) {
     myWarnings <<- c(myWarnings, list(w))
     invokeRestart("muffleWarning")
@@ -17,44 +20,55 @@ withWarnings <- function(expr) {
 collect_ya_errs <- function(e, fmt) {
   if (is.null(e)) return(NULL)
   vapply(e, "[[", character(1), "message")
-  # warn <- paste0(" ", warn, collapse = "\n  ....")
-  # paste(sprintf("  %s__:\n  ....%s", fmt, warn), collapse = "\n")
 }
 
-process_werrors <- function(warns, errs) {
-  alertfun <- function(nm, wrn, color = "yellow") {
+process_werrors <- function(warns, errs, dname) {
+
+  alertfun <- function(nm, wrn, dname, color = "yellow") {
+    nam <- sprintf("{.code %s}", nm)
+    dnam <- sprintf("in {.code %s}", dname)
+    wrn[[nm]] <- gsub("x[[i_x]]", nam, wrn[[nm]], fixed = TRUE)
+    wrn[[nm]] <- gsub("in g.", "in the global dictionary", wrn[[nm]], fixed = TRUE)
+    wrn[[nm]] <- gsub("in d.", dnam, wrn[[nm]], fixed = TRUE)
     cols <- c("yellow" = cli::symbol$warning, "red" = cli::symbol$error)
     sym  <- paste(cols[color], "")
+
     cli::cli_li(nm)
+
     dvid <- cli::cli_div(theme = list(span.emph = list(color = color, before = sym)))
+    on.exit(cli::cli_end(dvid), add = TRUE, after = FALSE)
+
     ulid <- cli::cli_ol()
+    on.exit(cli::cli_end(ulid), add = TRUE, after = FALSE)
+
     for (i in wrn[[nm]]){
-      cli::cli_li("{.emph {i}}")
+      the_warn <- sprintf("{.emph %s}", i)
+      cli::cli_li(the_warn)
     }
-    cli::cli_end(ulid)
-    cli::cli_end(dvid)
   }
+
   warns <- warns[lengths(warns) > 0]
-  errs  <- errs[lengths(errs) > 0]
-  warned <- length(warns) > 0
-  errored <- length(errs) > 0
+  errs  <-  errs[lengths(errs) > 0]
+
+  warned  <- length(warns) > 0
+  errored <-  length(errs) > 0
+
   if (warned || errored) {
   
     cli::cli_ul()
     if (warned) {
       cli::cli_h2("Warnings were found in the following columns")
       for (i in names(warns)) {
-        alertfun(i, warns)
+        alertfun(i, warns, dname)
       }
     }
     if (errored) {
       cli::cli_h2("Errors were found in the following columns")
       for (i in names(errs)) {
-        alertfun(i, errs, "red")
+        alertfun(i, errs, dname, "red")
       }
     }
     cli::cli_end()
-
   } else {
     NULL
   }
